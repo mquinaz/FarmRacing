4a76373e112795915082a0d0974da51bf0b16884
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-01-14 18:00:23 +0000

    Fixed sprite change

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <unistd.h>
 
 #define PRICEPLAY 1
 #define SIZESPRITES 7
-#define DURATIONANIMATION 0.5
+#define DURATIONANIMATION 0.1
+
 using namespace std;
 
 struct Player
 {
 	vector<sf::Texture> frames;
-	int size;
+	int size,currentFrame;
 	sf::Sprite x;
 };
 
@@ -24,9 +26,10 @@ struct Mapfarm
 int main()
 {	
     //macOS just won't agree if you try to create a window or handle events in a thread other than the main one.
-    //Windows doesn't like windows that are bigger than the desktop. This includes windows created with VideoMode::getDesktopMode(): with the window decorations (borders and titlebar) added, you end up with a window which is slightly bigger than the desktop.
-
-    sf::Clock programClock,playClock; 
+    //Windows doesn't like windows that are bigger than the desktop. This includes windows created with 
+    //VideoMode::getDesktopMode(): with the window decorations (borders and titlebar) added, you end up with a window which is slightly bigger than the desktop.
+    
+	sf::Clock programClock,playClock; 
     sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "Horse Racing");
     //window.setVerticalSyncEnabled(true);  //application will run same frequency of monitor's refresh rate
          	
@@ -106,11 +109,11 @@ int main()
 	
 	//defining the map (done previously using mouse click event to find coordinates
 	Mapfarm mapList;
-	mapList.coords.push_back( make_pair( 30,395) );
-	mapList.coords.push_back( make_pair( 30,501) );
-	mapList.coords.push_back( make_pair( 30,623) );
-	mapList.coords.push_back( make_pair( 30,708) );
-	mapList.coords.push_back( make_pair( 30,769) );
+	mapList.coords.push_back( make_pair( 30,350) );
+	mapList.coords.push_back( make_pair( 30,450) );
+	mapList.coords.push_back( make_pair( 30,550) );
+	mapList.coords.push_back( make_pair( 30,650) );
+	mapList.coords.push_back( make_pair( 30,750) );
 	
 	Player spriteList[SIZESPRITES];	
 	sf::Texture spriteTextures;
@@ -119,7 +122,8 @@ int main()
 	for(int i=1;i<=SIZESPRITES;i++)
 	{
 		string fileSpriteNameBeggining = "resource/";
-		int j=6;
+		//j is the number of frames (6 and 4 in dragon case), they are -- to later on use %
+		int j=5;
 		if(i == 1)
 			fileSpriteNameBeggining += "cat";
 		if(i == 2)
@@ -127,7 +131,7 @@ int main()
 		if(i == 3)
 		{
 			fileSpriteNameBeggining += "dragon";
-			j = 4;
+			j = 3;
 		}
 		if(i == 4)
 			fileSpriteNameBeggining += "pony";
@@ -151,8 +155,10 @@ int main()
 			spriteList[i-1].frames.push_back( spriteTextures );				
 		}
 		sf::Sprite spriteCharacter( spriteList[i-1].frames[0] );
+		spriteList[i-1].currentFrame = 0;
 		//spriteList[i-1].frames[i].setScale(ScaleX, ScaleY); 
 		spriteList[i-1].x = spriteCharacter;
+		spriteList[i-1].x.move( mapList.coords[i-1].first , mapList.coords[i-1].second);
 	}
 	
 	
@@ -160,8 +166,10 @@ int main()
     while (window.isOpen())
     {
         sf::Event event;
+        int playerOut;
+        sf::Time elapsedPlay;
         while (window.pollEvent(event))
-        {
+			{
             if (event.type == sf::Event::Closed)
                 window.close();
             if( event.type == sf::Event::MouseButtonPressed)
@@ -175,7 +183,7 @@ int main()
 				break;
 				
 			if (event.type == sf::Event::KeyPressed)
-			{
+			{	
 				if (event.key.code == sf::Keyboard::B)
 				{
 					std::cout << "the B key was pressed" << std::endl;
@@ -204,15 +212,25 @@ int main()
 						continue;
 					}
 					creditsIn -= PRICEPLAY;
-					flagPlay = true;				
+					elapsedPlay = playClock.restart();
+					flagPlay = true;			
+					plays++;
+					textStart.setString("PLAY: " + to_string(plays));
+					textCredits.setString( to_string(creditsIn) + " CREDITS IN / " + to_string(creditsOut) + " CREDITS OUT");
+			
+					//if possible improve this method - https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c
+					//player that is not playing this round
+					random_device dev;
+					mt19937 rng(dev());
+					uniform_int_distribution<mt19937::result_type> dist6(0,6); // distribution in range [0, 6]
+					playerOut = dist6(rng);
+					cout << playerOut << endl;	
 				}
 			}
         }
 
-        sf::Time elapsedPlay = playClock.restart();
-        //cout << elapsedPlay.asSeconds() << endl;
         window.clear();
-        
+
 		window.draw(spriteBackground_Image);
         window.draw(textStart);
 		window.draw(textCreditsIn);
@@ -222,29 +240,30 @@ int main()
 		
 		if(flagPlay)
 		{	
-			plays++;
-			textStart.setString("PLAY: " + to_string(plays));
-			textCredits.setString( to_string(creditsIn) + " CREDITS IN / " + to_string(creditsOut) + " CREDITS OUT");
-			
-			//if possible improve this method - https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c
-			random_device dev;
-			mt19937 rng(dev());
-			uniform_int_distribution<mt19937::result_type> dist6(0,6); // distribution in range [0, 6]
-			int playerOut = dist6(rng);
-			cout << playerOut << endl;
+			elapsedPlay = playClock.getElapsedTime();
+			cout << elapsedPlay.asSeconds() << endl;
+			if( DURATIONANIMATION <= elapsedPlay.asSeconds() )
+			{
+				for(int i=0;i<=6;i++)
+				{
+					int currentFrameAux = (spriteList[i].currentFrame + 1) % spriteList[i].size;
+					spriteList[i].currentFrame = currentFrameAux;
+					spriteList[i].x.setTexture( spriteList[i].frames[ spriteList[i].currentFrame ] );
+				}
+				elapsedPlay = playClock.restart();
+			}
 			
 			for(int i=0;i<=6;i++)
 			{
 				if( i == playerOut )
 					continue;
 
-				spriteList[i].x.move( mapList.coords[i].first , mapList.coords[i].second);
-				window.draw(spriteList[0].x);			
+				spriteList[i].x.move( 0.1 , 0);
+				window.draw(spriteList[i].x);			
 			}
-			
-			//flagPlay = false;	
-			continue;
+			//flagPlay = false;
 		}
+
 		
         window.display();
     }
